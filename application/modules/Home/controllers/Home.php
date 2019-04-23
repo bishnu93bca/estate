cf709f2b1161508333bd772dc265a598ee9de87c
Bishnu Ray <bishnu99bca@gmail.com>
2019-04-24 01:20:19 +0530

    commit line

--- application/modules/Home/controllers/Home.php
+++ application/modules/Home/controllers/Home.php
@@ -22,6 +22,7 @@ class Home extends MY_Controller {
 	    $keyword=$_GET['query'];
 	    if($_GET['query']){
 	    $data = file_get_contents('http://suggestqueries.google.com/complete/search?output=firefox&client=firefox&hl=en-US&q='.urlencode($keyword));
+	    // ==============
 	    if (($data = json_decode($data, true)) !== null) {
 	        $keywords = $data[1];
 	        header('Content-Type: application/json');
